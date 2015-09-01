e43768a8cf49e388c77bfa5d66ef08448a208669
crossgate <mugmickey@gmail.com>
2015-09-01 17:54:20 +0800

    More changes

--- baidu-tieba-auto-sign.py
+++ baidu-tieba-auto-sign.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import urllib
+
 import urllib2
 import cookielib
 import re
@@ -8,8 +8,12 @@ import json
 import threading
 import platform
 import os
-from bs4 import BeautifulSoup
+from sys import stdout
+# from bs4 import BeautifulSoup
 from pprint import pprint
+from urllib import quote
+from urllib import urlencode
+# from spincursor import SpinCursor
 
 
 def _setup_cookie(my_cookie):
@@ -17,90 +21,106 @@ def _setup_cookie(my_cookie):
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
     urllib2.install_opener(opener)
     opener.addheaders = [('User-agent',
-                          'Mozilla/5.0 (SymbianOS/9.3; Series60/3.2 NokiaE72-1/021.021; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/525 (KHTML, like Gecko) Version/3.0 BrowserNG/7.1.16352'),
+                          'Mozilla/5.0 (SymbianOS/9.3; Series60/3.2 NokiaE72-1/021.021;Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/525 (KHTML, like Gecko) Version/3.0 BrowserNG/7.1.16352'),
                          ('Cookie', my_cookie),
                          ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
 
-
 def _fetch_fav_pub_list():
-    print u'获取喜欢的贴吧ing...' if system_env else '获取喜欢的贴吧ing...'
+    # spin = SpinCursor(msg=u'获取喜欢的贴吧')
+    print u'获取喜欢的贴吧...' if system_env else '获取喜欢的贴吧...',
+    # spin.start()
     page_count = 1
     list_fav_pubs = []
+
     while True:
-        like_tieba_url = 'http://tieba.baidu.com/f/like/mylike?&pn=%d' % page_count  # retrieve pubs by page
-        req = urllib2.Request(like_tieba_url)
+        fav_pubs_list_page = 'http://tieba.baidu.com/f/like/mylike?&pn=%d' % page_count  # retrieve pubs by page
+        req = urllib2.Request(fav_pubs_list_page)
         resp = urllib2.urlopen(req).read()
-        html_doc = resp.decode('gbk').encode('utf8')
-        # fav_pub = "<a href=\"\/f\?kw=.*?\" title=\"(.*?)\">.+?<\/a><\/td><td><a class=\"cur_exp\" target=\"_blank\".*?"
-        # fav_pubs_of_this_page = re.findall(fav_pub, resp)
-        fav_pubs_of_this_page = []
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        # print soup
+        pub_page = resp.decode('gbk').encode('utf8')
+        # pub_page = resp.decode('gbk')
+        # pub_page = resp
+        title_list_re = "<a href=\"\/f\?kw=.*?\" title=\"(.*?)\">.+?<\/a><\/td><td><a class=\"cur_exp\" target=\"_blank\".*?"
+        title_list_on_page = re.findall(title_list_re, pub_page)
+        '''fav_pubs_of_this_page = []
+        soup = BeautifulSoup(pub_page, 'html.parser')
         tableRows = soup.find('div', {'class': 'forum_table'}).find('table').findAll('tr')
         for tr in tableRows:
             cols = tr.findAll('td')
             # pprint(cols)
             if len(cols) >= 4:
-                link = cols[0].find('a').get('title')
+                # link = cols[0].find('a').get('href')
+                tmp_title = cols[0].find('a').get('title')
                 # link_array = link.split('=')
                 # fav_pub = link_array[1]
-                fav_pub = link.encode('utf-8"')
-                # print fav_pub.decode("utf-8")
-                fav_pubs_of_this_page.append(fav_pub)
-        # pprint(fav_pubs_of_this_page)
-        if not fav_pubs_of_this_page:
+                fav_pub = tmp_title.encode('utf-8')
+                # print type(fav_pub)
+                # print fav_pub.decode('utf-8')
+                fav_pubs_of_this_page.append(fav_pub)'''
+        if not title_list_on_page:
             break
-        if not list_fav_pubs:
-            list_fav_pubs = fav_pubs_of_this_page
         else:
-            list_fav_pubs += fav_pubs_of_this_page
+            list_fav_pubs += title_list_on_page
         page_count += 1
 
+    '''for item in list_fav_pubs:
+        print repr(item)
+        print type(item)'''
+    # spin.stop()
+    print ("\r"),
     return list_fav_pubs
 
 
-def _fetch_tieba_info(tieba):
-    tieba_wap_url = "http://tieba.baidu.com/mo/m?kw=" + tieba
-    wap_resp = urllib2.urlopen(tieba_wap_url).read()
+def _fetch_pub_info(pub_name):
+    pub_wap_url = "http://tieba.baidu.com/mo/m?kw=" + quote(pub_name)
+    wap_resp = urllib2.urlopen(pub_wap_url).read()
 
     if not wap_resp:
         return
     re_already_sign = '<td style="text-align:right;"><span[ ]>(.*?)<\/span><\/td><\/tr>'
     already_sign = re.findall(re_already_sign, wap_resp)
+    if already_sign:
+        already_sign = already_sign[0]
+        # print type(already_sign), already_sign.decode('utf-8')
+    # print type(already_sign), already_sign.decode('utf-8')
 
     re_fid = '<input type="hidden" name="fid" value="(.+?)"\/>'
     _fid = re.findall(re_fid, wap_resp)
     fid = _fid and _fid[0] or None
+    # print type(fid), fid
 
     re_tbs = '<input type="hidden" name="tbs" value="(.+?)"\/>'
     _tbs = re.findall(re_tbs, wap_resp)
-
     tbs = _tbs and _tbs[0] or None
+    # print type(tbs), tbs
+
     return already_sign, fid, tbs
 
 
-def _decode_uri_post(postData):
-    SIGN_KEY = "tiebaclient!!!"
+def _decode_uri_post(post_data):
+    sign_key = "tiebaclient!!!"
     s = ""
-    keys = postData.keys()
+    keys = post_data.keys()
     keys.sort()
     for i in keys:
-        s += i + '=' + postData[i]
-    sign = hashlib.md5(s + SIGN_KEY).hexdigest().upper()
-    postData.update({'sign': str(sign)})
-    return postData
+        s += i + '=' + post_data[i]
+    sign_stamp = hashlib.md5(s + sign_key).hexdigest().upper()
+    post_data.update({'sign': str(sign_stamp)})
+    return post_data
 
 
-def _make_sign_request(pub_name, fid, tbs, BDUSS):
+def _make_sign_request(pub_name, fid, tbs, bduss):
     sign_url = 'http://c.tieba.baidu.com/c/c/forum/sign'
-    sign_request = {"BDUSS": BDUSS,
+    sign_request = {"BDUSS": bduss,
                     "_client_id": "03-00-DA-59-05-00-72-96-06-00-01-00-04-00-4C-43-01-00-34-F4-02-00-BC-25-09-00-4E-36",
-                    "_client_type":
-                        "4", "_client_version": "1.2.1.17", "_phone_imei": "540b43b59d21b7a4824e1fd31b08e9a6",
-                    "fid": fid, "kw": pub_name, "net_type": "3", 'tbs': tbs}
-
+                    "_client_type": "4",
+                    "_client_version": "1.2.1.17",
+                    "_phone_imei": "540b43b59d21b7a4824e1fd31b08e9a6",
+                    "fid": fid,
+                    "kw": pub_name,
+                    "net_type": "3",
+                    'tbs': tbs}
     sign_request = _decode_uri_post(sign_request)
-    sign_request = urllib.urlencode(sign_request)
+    sign_request = urlencode(sign_request)
 
     sign_request = urllib2.Request(sign_url, sign_request)
     sign_request.add_header(
@@ -108,58 +128,59 @@ def _make_sign_request(pub_name, fid, tbs, BDUSS):
     return sign_request
 
 
-def _handle_response(sign_resp):
+def _handle_response(sign_resp, pub_name):
     sign_resp = json.load(sign_resp)
     error_code = sign_resp['error_code']
     sign_bonus_point = 0
     try:
         # Don't know why but sometimes this will trigger key error.
-        sign_bonus_point = int(sign_resp['user_info']['sign_bonus_point'])
+        if 'user_info' in sign_resp:
+            if 'sign_bonus_point' in sign_resp['user_info']:
+                sign_bonus_point = int(sign_resp['user_info']['sign_bonus_point'])
     except KeyError:
         pass
     if error_code == '0':
-        print u"签到成功,经验+%d" % sign_bonus_point if system_env else "签到成功,经验+%d" % sign_bonus_point
+        print pub_name.decode('utf-8') + u"签到成功,经验+%d" % sign_bonus_point if system_env else pub_name + "签到成功,经验+%d" % sign_bonus_point
     else:
-        error_msg = sign_resp['error_msg']
-        if error_msg == u'亲，你之前已经签过了':
-            print u'之前已签到' if system_env else '之前已签到'
-        else:
-            print u'签到失败' if system_env else '签到失败'
-            print "Error:" + unicode(error_code) + " " + unicode(error_msg)
+        if 'error_msg' in sign_resp:
+            error_msg = sign_resp['error_msg']
+            print pub_name.decode('utf-8') + ":" if system_env else pub_name + ":",
+            print "(Error:" + unicode(error_code) + ") " + unicode(error_msg)
 
 
-def _sign_tieba(pub_name, BDUSS):
-    already_sign, fid, tbs = _fetch_tieba_info(pub_name)
+def _sign_a_pub(pub_name, bduss):
+    already_sign, fid, tbs = _fetch_pub_info(pub_name)
     if not already_sign:
-        print pub_name.decode('utf-8') + u'......正在尝试签到' if system_env else pub_name + '......正在尝试签到'
+        print pub_name.decode('utf-8') + u': 正在尝试签到' if system_env else pub_name + ': 正在尝试签到'
     else:
-        if already_sign[0] == "已签到":
-            print pub_name.decode('utf-8') + u"......之前已签到" if system_env else pub_name + "......之前已签到"
+        if already_sign == "已签到":
+            print pub_name.decode('utf-8') + u": 已签到" if system_env else pub_name + ": 已签到"
             return
 
     if not fid or not tbs:
         print u"签到失败，原因未知" if system_env else "签到失败，原因未知"
         return
 
-    sign_request = _make_sign_request(pub_name, fid, tbs, BDUSS)
+    sign_request = _make_sign_request(pub_name, fid, tbs, bduss)
     sign_resp = urllib2.urlopen(sign_request, timeout=5)
-    _handle_response(sign_resp)
+    _handle_response(sign_resp, pub_name)
 
 
-def sign(my_cookie, BDUSS):
+def sign(my_cookie, _bduss):
     _setup_cookie(my_cookie)
     _fav_pub_list = _fetch_fav_pub_list()
     if len(_fav_pub_list) == 0:
         print u"获取喜欢的贴吧失败，请检查Cookie和BDUSS是否正确" if system_env else "获取喜欢的贴吧失败，请检查Cookie和BDUSS是否正确"
         return
+
     thread_list = []
     for pub in _fav_pub_list:
-        t = threading.Thread(target=_sign_tieba, args=(pub, BDUSS))
+        t = threading.Thread(target=_sign_a_pub, args=(pub, _bduss))
         thread_list.append(t)
         t.start()
 
     for t in thread_list:
-        t.join(2)
+        t.join(3)
 
 
 def main():
@@ -170,10 +191,12 @@ def main():
     my_cookie_list = my_cookie.split('; ')
     for item in my_cookie_list:
         if "BDUSS=" in item:
-            mBDUSS = item
-    if not mBDUSS:
+            mbduss_str = item
+    if not mbduss_str:
         exit(0)
-    sign(my_cookie, mBDUSS)
+    mbduss_list = mbduss_str.split('BDUSS=')
+    mbduss = mbduss_list[1]
+    sign(my_cookie, mbduss)
 
 
 if __name__ == "__main__":
